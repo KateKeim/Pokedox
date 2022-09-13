a5331ce55806a85eb827564f08f7c3fce2abd934
KateCode <110600575+KateKeim@users.noreply.github.com>
2022-09-13 12:04:05 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+let characterList = []; //This array contains Character data from Stardew Valley game to display in application.
+//array of objects
+let character = [{
+  name: 'Alex', 
+  Bithday: 13, 
+  gift: 'Complete Breakfast'
+},
+  {
+  name: 'Elliott', 
+  Bithday: 5, 
+  gift:'Crap Cakes'
+},
+  {
+  name: 'Harvey', 
+  Bithday: 14, 
+  gift: 'Coffee'
+},
+  {
+  name: 'Sam', 
+  Bithday: 17, 
+  gift: 'Cactus Fruit'
+},
+  {
+  name: 'Sebastian', 
+  Bithday: 10, 
+  gift: 'Frozen Tear'
+},
+  {
+  name: 'Shane', 
+  Bithday: 20, 
+  gift: 'Beer' 
+}]

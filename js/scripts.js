65c39253ca84dd92272026853714f67aec09851b
KateCode <110600575+KateKeim@users.noreply.github.com>
2022-09-14 10:04:27 +0200

    Edit dictionary

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,11 @@
-let characterList = []; //This array contains Character data from Stardew Valley game to display in application.
+//This array contains Character data from Stardew Valley game to display in application.
+let characterList = []
 //array of objects
-let character = [{
-  name: 'Alex', 
-  Bithday: 13, 
-  gift: 'Complete Breakfast'
-},
-  {
-  name: 'Elliott', 
-  Bithday: 5, 
-  gift:'Crap Cakes'
-},
-  {
-  name: 'Harvey', 
-  Bithday: 14, 
-  gift: 'Coffee'
-},
-  {
-  name: 'Sam', 
-  Bithday: 17, 
-  gift: 'Cactus Fruit'
-},
-  {
-  name: 'Sebastian', 
-  Bithday: 10, 
-  gift: 'Frozen Tear', 'Obsidian'
-},
-  {
-  name: 'Shane', 
-  Bithday: 20, 
-  gift: 'Beer' 
-}]
+{'characterList':[
+  'name': 'Alex', 'Bithday': 13, 'gift': 'Complete Breakfast',
+  'name': 'Elliott', 'Bithday': 5, 'gift':'Crap Cakes',
+  'name': 'Harvey','Bithday': 14, 'gift': 'Coffee',
+  'name': 'Sam', 'Bithday': 17, 'gift': 'Cactus Fruit',
+  'name': 'Sebastian', 'Bithday': 10, 'gift': 'Frozen Tear', 'Obsidian',
+  'name': 'Shane', 'Bithday': 20, 'gift': 'Beer'
+]}

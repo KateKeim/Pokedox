1f66d183437854da65ad01ef8d4480fca4f7f85f
KateCode <110600575+KateKeim@users.noreply.github.com>
2022-09-14 17:27:38 +0200

    Edit liist objects

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,6 @@ let characterList = []
   'name': 'Elliott', 'Bithday': 5, 'gift':'Crap Cakes',
   'name': 'Harvey','Bithday': 14, 'gift': 'Coffee',
   'name': 'Sam', 'Bithday': 17, 'gift': 'Cactus Fruit',
-  'name': 'Sebastian', 'Bithday': 10, 'gift': 'Frozen Tear', 'Obsidian',
+  'name': 'Sebastian', 'Bithday': 10, 'gift': ['Frozen Tear', 'Obsidian',]
   'name': 'Shane', 'Bithday': 20, 'gift': 'Beer'
 ]}

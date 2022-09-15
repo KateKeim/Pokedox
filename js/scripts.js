8b63e3e870ff537c7e3bc8db0ae1ea5e01c56847
KateCode <110600575+KateKeim@users.noreply.github.com>
2022-09-15 09:26:04 +0200

    Edit characterList

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,32 @@
-//This array contains Character data from Stardew Valley game to display in application.
-let characterList = []
-//array of objects
-{'characterList':[
-  'name': 'Alex', 'Bithday': 13, 'gift': 'Complete Breakfast',
-  'name': 'Elliott', 'Bithday': 5, 'gift':'Crap Cakes',
-  'name': 'Harvey','Bithday': 14, 'gift': 'Coffee',
-  'name': 'Sam', 'Bithday': 17, 'gift': 'Cactus Fruit',
-  'name': 'Sebastian', 'Bithday': 10, 'gift': ['Frozen Tear', 'Obsidian',]
-  'name': 'Shane', 'Bithday': 20, 'gift': 'Beer'
-]}
+let characterList = [
+{
+'name': 'Alex',
+'Bithday': 13, 'gift':
+'Complete Breakfast'
+},
+{
+'name': 'Elliott',
+'Bithday': 5,
+'gift':'Crap Cakes'
+},
+{
+'name': 'Harvey',
+'Bithday': 14,
+'gift': 'Coffee'
+},
+{
+'name': 'Sam',
+'Bithday': 17,
+'gift': 'Cactus Fruit'
+},
+{
+'name': 'Sebastian',
+'Bithday': 10,
+'gift': ['Frozen Tear', 'Obsidian']
+},
+{
+'name': 'Shane',
+'Bithday': 20,
+'gift': 'Beer'
+}
+]

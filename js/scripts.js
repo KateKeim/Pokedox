5489fda5356916c22b3f3cdbb59f3b30264f8dc6
KateCode <110600575+KateKeim@users.noreply.github.com>
2022-10-04 20:47:52 +0200

    Add note to Harvey

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,52 @@
+//This array contains Character data from Stardew Valley game to display in application.
 let characterList = [
-{
-'name': 'Alex',
-'Bithday': 13, 'gift':
-'Complete Breakfast'
-},
-{
-'name': 'Elliott',
-'Bithday': 5,
-'gift':'Crap Cakes'
-},
-{
-'name': 'Harvey',
-'Bithday': 14,
-'gift': 'Coffee'
-},
-{
-'name': 'Sam',
-'Bithday': 17,
-'gift': 'Cactus Fruit'
-},
-{
-'name': 'Sebastian',
-'Bithday': 10,
-'gift': ['Frozen Tear', 'Obsidian']
-},
-{
-'name': 'Shane',
-'Bithday': 20,
-'gift': 'Beer'
-}
-]
+    {
+    "name": 'Alex',
+    "Birthday": 13,
+    "gift":'Complete Breakfast'
+    },
+    {
+    "name": 'Elliott',
+    "Birthday": 5,
+    "gift":'Crap Cakes'
+    },
+    {
+    "name": 'Harvey',
+    "Birthday": 14,
+    "gift": 'Coffee'
+    },
+    {
+    "name": 'Sam',
+    "Birthday": 17,
+    "gift": 'Cactus Fruit'
+    },
+    {
+    "name": 'Sebastian',
+    "Birthday": 10,
+    "gift": ['Frozen Tear', 'Obsidian']
+    },
+    {
+    "name": 'Shane',
+    "Birthday": 20,
+    "gift": 'Beer'
+    }
+    ]
+    
+    let open = '('
+    let close = ')'
+    let character = ''; // declare a empty variable
+    let comment = 'He is a doctor!'; //note to add
+    let birthday = characterList.Birthday;
 
-let open = '('
-let close = ')'
-
-let character = ''; // declare a empty variable
-// for loop that iterates each character in the above array
-for(let i = 0; i < characterList.length; i++) {
-// added condition for the character length > 5
-if (characterList.length > 5) {
-character = <p class='character-list'>${characterList[i].name} ${open} Birthday: ${characterList[i].Birthday} ${close} ${characterList[i].gift};</p>
-} else {
-document.write(<p class='character-list'>${characterList[i].name} ${open} Birthday: ${characterList[i].Birthday}${close}</p>);
-}
-}
+    // for loop that iterates each character in the above array
+    for(let i = 0; i < characterList.length; i++) {
+    // added condition for the character length > 5
+    if (characterList[i].Birthday == 14) {
+    character = `<p class='character-list'>${characterList[i].name} 
+        ${open} Birthday: ${characterList[i].Birthday} ${close} ${comment} </p>`;
+    document.write(character);
+    } else {
+    document.write(`<p class='character-list'>${characterList[i].name} 
+        ${open} Birthday: ${characterList[i].Birthday}${close}</p>`);
+    }
+    }

5f77ae4a5b8c70789da1840bc87f4287542bd8ec
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-01-06 14:47:05 +0100

    1.5 Part1 forEach() Loops
    restart the exercise again. This is Task 1.5 Part1

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,40 @@
 //This array contains Character data from Stardew Valley game to display in application.
-//Wrap array to local scope
 
-let characterRepository = (function () {
-let characterList = [
-    {
-    "name": 'Alex',
-    "Birthday": 13,
-    "gift":'Complete Breakfast'
-    },
-    {
-    "name": 'Elliott',
-    "Birthday": 5,
-    "gift":'Crap Cakes'
-    },
-    {
-    "name": 'Harvey',
-    "Birthday": 14,
-    "gift": 'Coffee'
-    },
-    {
-    "name": 'Sam',
-    "Birthday": 17,
-    "gift": 'Cactus Fruit'
-    },
-    {
-    "name": 'Sebastian',
-    "Birthday": 10,
-    "gift": ['Frozen Tear', 'Obsidian']
-    },
-    {
-    "name": 'Shane',
-    "Birthday": 20,
-    "gift": 'Beer'
-    }
+    let characterList = [
+        {
+            "name": 'Alex',
+            "Birthday": 13,
+            "gift": 'Complete Breakfast'
+        },
+        {
+            "name": 'Elliott',
+            "Birthday": 5,
+            "gift": 'Crap Cakes'
+        },
+        {
+            "name": 'Harvey',
+            "Birthday": 14,
+            "gift": 'Coffee'
+        },
+        {
+            "name": 'Sam',
+            "Birthday": 17,
+            "gift": 'Cactus Fruit'
+        },
+        {
+            "name": 'Sebastian',
+            "Birthday": 10,
+            "gift": ['Frozen Tear', 'Obsidian']
+        },
+        {
+            "name": 'Shane',
+            "Birthday": 20,
+            "gift": 'Beer'
+        }
     ];
 
-    characterList.forEach(character => console.log(character));
-
-    //add a character to the characterLst array
-    return {
-        add: function(character) {
-            characterList.push(character);
-        },
-        getAll: function() {
-            return characterList;
-        }
-    };
-})();
-
-console.log(characterRepository.getAll());
-characterRepository.add({ name: 'Lewis'});
-console.log(characterRepository.getAll());
+    //1.5 Task part 1: use forEach instead of for
+    characterList.forEach(function(character){
+        document.write(character.name +' ' + 'Birthday ' + character.Birthday + ' ' + 'Gift for him: ' + character.gift + '<br>');
+    })
 

cd74e5d576709e756e80e3cc91d4843d6063fc94
KateKeim <110600575+KateKeim@users.noreply.github.com>
2023-01-06 16:41:04 +0100

    Exercise 1.5 Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 //This array contains Character data from Stardew Valley game to display in application.
 
-    let characterList = [
+//Exercise 1.5 Part2.2: create Repository to hold IIFE
+let characterRepository = (function(){  
+//Exercise 1.5 Part 2.1: wrap characterList in an IIFE      
+let characterList = 
+    [
         {
             "name": 'Alex',
             "Birthday": 13,
@@ -33,8 +37,47 @@
         }
     ];
 
-    //1.5 Task part 1: use forEach instead of for
-    characterList.forEach(function(character){
-        document.write(character.name +' ' + 'Birthday ' + character.Birthday + ' ' + 'Gift for him: ' + character.gift + '<br>');
-    })
+//1.5 Task part 1: use forEach instead of for
+    /*characterList.forEach(function(character){
+        document.write('Name: ' + character.name +'<br> ' + 'Birthday ' + character.Birthday + '<br> ' + 'Gift for him: ' + character.gift + '<br>');
+    })*/
 
+//Exercise 1.5 Part 2.3: assigned key getAll = show characterList.array
+
+function getAll(){
+    return characterList;
+}
+
+//Exercise 1.5 Part 2.3: assigned key add = add an new item
+    function add(character){
+        characterList.push(character);
+    }
+
+
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+console.log(characterRepository.getAll());
+characterRepository.add({name: 'Lewis'});
+console.log(characterRepository.getAll());
+
+//Exercise 1.5 Part 2.4: make sure both function are defined sepelately
+console.log(characterRepository.getAll());
+console.log(characterRepository.add());
+
+//Exercise 1.5 Part 2.5:Outside of and below the IIFE, have a forEach() loop
+let characterList = characterRepository.getAll();
+
+characterList.forEach(printDetails);
+function printDetails (character) {
+    let highlight = '';
+    if (character.Birthday == 14) {
+        highlight = ' - He is a doctor!';
+    }
+    document.write('Name: ' + character.name +  highlight + '<br> ' + 'Birthday ' + character.Birthday + '<br> ' + 'Gift for him: ' + character.gift + '<br>' + '<br>')
+}
